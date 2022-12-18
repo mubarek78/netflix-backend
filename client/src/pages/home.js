664193ec57f8725a5d83ffa178174d39bc6fe739
mubarek78 <70493771+mubarek78@users.noreply.github.com>
2022-12-17 20:02:03 -0700

    updated

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -11,14 +11,7 @@ function Home({type}) {
       <>
          <Nav />
         <Banner type={type}/>
-        {/* <Row title="Netflix original" fetchUrl={requests.fetchNetflixOriginals} isLarge={true} />
-        <Row title="Trending" fetchUrl={requests.fetchTrending}/>
-        <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies}/>
-        <Row title="Action" fetchUrl={requests.fetchActionMovies}/>
-        <Row title="Comedy" fetchUrl={requests.fetchComedyMovies}/>
-        <Row title="Horor" fetchUrl={requests.fetchHororMovies}/>
-        <Row title="Romance" fetchUrl={requests.fetchRomanceMovies}/>
-        <Row title="Documentary" fetchUrl={requests.fetchDocumentaryMovies}/> */}
+        
    
         <List title="Netflix original" fetchUrl={requests.fetchNetflixOriginalMovies} isLarge={true} />
         <List title="Trending" fetchUrl={requests.fetchTrendingMovies}/>
